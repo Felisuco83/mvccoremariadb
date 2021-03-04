31db41a6a6020ebcf55e6d9287429897d85826fe
Serra <pacoserranox@gmail.com>
2021-03-04 19:37:30 +0100

    commit;

--- MvcCoreMariaDb/Startup.cs
+++ MvcCoreMariaDb/Startup.cs
@@ -5,21 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcCoreMariaDb.Data;
+using MvcCoreMariaDb.Repositories;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Pomelo.EntityFrameworkCore.MySql.Storage;
 
 namespace MvcCoreMariaDb
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        { 
+            this.Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            String cadena = this.Configuration.GetConnectionString("cadenamariadb");
+            services.AddTransient<RepositoryComics>();
+            services.AddDbContext<ComicContext>(options => options
+                .UseMySql(cadena, ServerVersion.AutoDetect(cadena)));
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
