9a050981e1b7c6c733ef737e4b7ad8a435dc17e7
Serra <pacoserranox@gmail.com>
2021-03-04 21:03:20 +0100

    commit

--- MvcCoreMariaDb/obj/Debug/netcoreapp3.1/MvcCoreMariaDb.AssemblyInfo.cs
+++ MvcCoreMariaDb/obj/Debug/netcoreapp3.1/MvcCoreMariaDb.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("20f29b19-bf86-4646-9ab3-e6b977e3f017")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcCoreMariaDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
